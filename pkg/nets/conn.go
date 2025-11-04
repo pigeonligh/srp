13b03e7f16752bc99c0f988871a109b1e863de0d
pigeonligh <pigeonligh@hotmail.com>
2025-11-04 17:14:10 +0800

    improve connection handle

--- pkg/nets/conn.go
+++ pkg/nets/conn.go
@@ -2,37 +2,52 @@ package nets
 
 import (
 	"io"
+	"sync"
 
 	"golang.org/x/sync/errgroup"
 )
 
+var DefaultCopyBufferSize = 8192
+
 func IOCopy(dst io.Writer, src io.Reader) error {
-	_, err := io.CopyBuffer(dst, src, make([]byte, 8192))
+	_, err := io.CopyBuffer(dst, src, make([]byte, DefaultCopyBufferSize))
 	return err
 }
 
 func HandleConnections(c1, c2 io.ReadWriteCloser) error {
+	var o sync.Once
+	cleanup := func() {
+		o.Do(func() {
+			_ = c1.Close()
+			_ = c2.Close()
+		})
+	}
+	defer cleanup()
+
+	handleDirect := func(w io.Writer, r io.Reader) error {
+		err := IOCopy(w, r)
+		if err != nil && err != io.EOF {
+			cleanup() // 如果一端出错，关闭连接
+		} else {
+			ConnCloseWrite(w) // 正常结束时，关闭写端
+		}
+		return err
+	}
+
 	var pipes errgroup.Group
 	pipes.Go(func() error {
-		err := IOCopy(c1, c2)
-		SafeCloseConn(c1)
-		return err
+		return handleDirect(c1, c2)
 	})
 	pipes.Go(func() error {
-		err := IOCopy(c2, c1)
-		SafeCloseConn(c2)
-		return err
+		return handleDirect(c2, c1)
 	})
-
 	return pipes.Wait()
 }
 
-func SafeCloseConn(c io.ReadWriteCloser) {
+func ConnCloseWrite(c any) {
 	if cw, ok := c.(interface {
 		CloseWrite() error
 	}); ok {
 		_ = cw.CloseWrite()
-	} else {
-		_ = c.Close()
 	}
 }
